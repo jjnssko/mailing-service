2ce2fe452133d5a495627c1ef6d021eaafbb51b1
Jonáš Vysocký <jonas.vysocky@gmail.com>
2024-11-01 15:59:08 +0100

    Remove unnecessary stuff for API

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    /*Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],*/
 ];
