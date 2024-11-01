5c5883e132de88a6161ffd253dfbac35398f0d82
Jonas Vysocky <75484350+jjnssko@users.noreply.github.com>
2024-11-01 16:01:30 +0100

    Add symfony/monolog-bundle without unnecessary front-end stuff (#4)
    * Add symfony/monolog-bundle
    
    * Remove unnecessary stuff for API

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,8 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    /*Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],*/
 ];
