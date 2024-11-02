29a67bbfe1ce1589399a010fed97d8326f25cb78
Jonáš Vysocký <jonas.vysocky@gmail.com>
2024-11-02 09:37:32 +0100

    Rollup migrations

--- migrations/Version20241102083707.php
+++ migrations/Version20241102083707.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20241006152707 extends AbstractMigration
+final class Version20241102083707 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -18,8 +18,8 @@ final class Version20241006152707 extends AbstractMigration
     {
         $this->addSql('CREATE TABLE email_process_log (id INT AUTO_INCREMENT NOT NULL, user_token_id INT NOT NULL, sender_name VARCHAR(255) NOT NULL, sender_email VARCHAR(255) NOT NULL, subject LONGTEXT NOT NULL, body LONGTEXT NOT NULL, response_code INT NOT NULL, error_message LONGTEXT DEFAULT NULL, INDEX IDX_A53D0B31A15303B9 (user_token_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE email_receiver (id INT AUTO_INCREMENT NOT NULL, user_token_id INT NOT NULL, full_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, INDEX IDX_36DE93DFA15303B9 (user_token_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user_token (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, type VARCHAR(255) NOT NULL, related_url VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, last_usage DATE DEFAULT NULL, INDEX IDX_BDF55A63A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_token (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, type VARCHAR(255) NOT NULL, related_url VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, last_usage DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_BDF55A63A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE email_process_log ADD CONSTRAINT FK_A53D0B31A15303B9 FOREIGN KEY (user_token_id) REFERENCES user_token (id)');
         $this->addSql('ALTER TABLE email_receiver ADD CONSTRAINT FK_36DE93DFA15303B9 FOREIGN KEY (user_token_id) REFERENCES user_token (id)');
         $this->addSql('ALTER TABLE user_token ADD CONSTRAINT FK_BDF55A63A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
